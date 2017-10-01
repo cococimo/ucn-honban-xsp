8ca6d4e4612da977c53e541a32713cc061938bf9
t_yoshida <t_yoshida@eltex.co.jp>
2017-10-02 08:30:09 +0900

    【エルテックス】LPカートに入れる対応

---
+++ honban/contents/js/staticCartSkuAdd.js
@@ -0,0 +1,173 @@
+//siteCatalistAddCart
+function AddCart_trans(){
+   document.cartGoodsAddForm.order_num_string.value=document.goodsDetailCartForm.order_num_string.value;
+  return anchorButtonClick(document.cartGoodsAddForm);
+}
+function siteCatalistAddCart(s_products){
+  s.linkTrackVars='events,products';
+  s.linkTrackEvents='scAdd,event27';
+  s.products=";"+s_products;
+  s.events='scAdd,event27';
+  s.tl(this,'o','cartadd');
+  setTimeout("AddCart_trans()",1000);
+}
+function AddCart_trans1(){
+//  document.cartGoodsAddForm1.order_num_string.value=document.goodsDetailCartForm1.order_num_string.value;
+  return anchorButtonClick(document.cartGoodsAddForm1);
+}
+function siteCatalistAddCart1(s_products){
+  s.linkTrackVars='events,products';
+  s.linkTrackEvents='scAdd,event27';
+  s.products=";"+s_products;
+  s.events='scAdd,event27';
+  s.tl(this,'o','cartadd');
+  setTimeout("AddCart_trans1()",1000);
+}
+//siteCatalistAddCart SKU
+function showCartButton(obj){
+  var idx = obj.selectedIndex;
+  var goods_sku_code = obj.options[idx].value;
+  var cartButton = document.getElementsByClassName("cartButton");
+  var selectButton = document.getElementsByClassName("selectButton");
+  if( idx != 0 ){
+      cartButton[0].style.display='inline';
+      selectButton[0].style.display='none';
+  }else{
+    cartButton[0].style.display='none';
+    selectButton[0].style.display='inline';
+  }
+}
+function AddCart_trans_renewal(){
+   document.cartGoodsAddForm.order_num_string.value=document.goodsDetailCartForm.order_num_string.value;
+   document.cartGoodsAddForm.goods_sku_code.value=document.goodsDetailCartForm.goods_sku_code.value;
+  return anchorButtonClick(document.cartGoodsAddForm);
+}
+function siteCatalistAddCart_renewal(s_products){
+    s.linkTrackVars='events,products';
+    s.linkTrackEvents='scAdd,event27';
+    s.products=";"+s_products;
+    s.events='scAdd,event27';
+    s.tl(this,'o','cartadd');
+    setTimeout("AddCart_trans_renewal()",1000);
+}
+function AddCart_trans1_renewal(){
+   document.cartGoodsAddForm1.order_num_string.value=document.goodsDetailCartForm1.order_num_string.value;
+   document.cartGoodsAddForm1.goods_sku_code.value=document.goodsDetailCartForm1.goods_sku_code.value;
+  return anchorButtonClick(document.cartGoodsAddForm1);
+}
+function siteCatalistAddCart1_renewal(s_products,static_){
+    s.linkTrackVars='events,products';
+    s.linkTrackEvents='scAdd,event27';
+    s.products=";"+s_products;
+    s.events='scAdd,event27';
+    s.tl(this,'o','cartadd');
+    setTimeout("AddCart_trans1_renewal()",1000);
+}
+function AddCart_trans2(formName){
+  return anchorButtonClick(formName);
+}
+function siteCatalistAddCart2(formName,s_products){
+  s.linkTrackVars='events,products';
+  s.linkTrackEvents='scAdd,event27';
+  s.products=";"+s_products;
+  s.events='scAdd,event27';
+  s.tl(this,'o','cartadd');
+  setTimeout('AddCart_trans2('+ formName +')',1000);
+}
+function getSkuGoodsParam(formName,displayCD,goodsSkuCD,extGoodsSkuCD){
+	document.forms[formName].display_code.value = displayCD;
+	document.forms[formName].goods_sku_code.value = goodsSkuCD;
+	document.forms[formName].ext_goods_sku_code.value = extGoodsSkuCD;
+	document.forms[formName].order_num_string.value = 1;
+	siteCatalistAddCart2(formName,extGoodsSkuCD);
+}
+
+function getSkuGoodsParamMono(formName){
+	document.forms[formName].order_num_string.value = 1;
+	siteCatalistAddCart2(formName,document.forms[formName].ext_goods_sku_code.value);
+}
+
+function cartAddSkuForRadio(){
+	var radioList = document.getElementsByName("sku_radio");
+	var paramList = "";
+	for(var i=0; i<radioList.length; i++){
+			if (radioList[i].checked) {
+				
+				paramList = document.getElementById("sku_radio" + i).value;
+				break;
+			}
+	}
+	var cartAddRadioParam= paramList.split(",");
+	getSkuGoodsParam(cartAddRadioParam[0],cartAddRadioParam[1],cartAddRadioParam[2],cartAddRadioParam[3]);
+}
+function AddCart_trans_static(){
+  document.cartGoodsAddForm.action="https://" + document.domain + "/shop/cartGoodsAdd.do";
+  return anchorButtonClick(document.cartGoodsAddForm);
+}
+function siteCatalistAddCart_static(s_products,static_goods_sku_code,static_display_code){
+    document.cartGoodsAddForm.order_num_string.value="1";
+    document.cartGoodsAddForm.goods_sku_code.value=static_goods_sku_code;
+    document.cartGoodsAddForm.display_code.value=static_display_code;
+    document.cartGoodsAddForm.ext_goods_sku_code.value=s_products;
+    s.linkTrackVars='events,products';
+    s.linkTrackEvents='scAdd,event27';
+    s.products=";"+s_products;
+    s.events='scAdd,event27';
+    s.tl(this,'o','cartadd');
+    setTimeout("AddCart_trans_static()",1000);
+}
+function siteCatalistAddCart_skuStatic(static_goods_sku_code,static_display_code){
+	if(!static_goods_sku_code){
+		alert("色・サイズを選択してください。");
+		return;
+	}
+    document.cartGoodsAddForm.order_num_string.value="1";
+    document.cartGoodsAddForm.goods_sku_code.value=static_goods_sku_code;
+    document.cartGoodsAddForm.display_code.value=static_display_code;
+    document.cartGoodsAddForm.ext_goods_sku_code.value=document.getElementById(static_goods_sku_code).value;
+    s.linkTrackVars='events,products';
+    s.linkTrackEvents='scAdd,event27';
+    s.products=";"+document.getElementById(static_goods_sku_code).value;
+    s.events='scAdd,event27';
+    s.tl(this,'o','cartadd');
+    setTimeout("AddCart_trans_static()",1000);
+}
+function cartAddSkuForRadio_static(cartGoodsAddForm_staticName){
+	var radioListForm = document.getElementById(cartGoodsAddForm_staticName);
+	var paramList = "";
+	var radioList = radioListForm.sku_radio;
+	for(var i=0; i<radioList.length; i++){
+		if (radioList[i].checked) {
+			
+			paramList = radioList[i].value;
+			break;
+		}
+		if(i==radioList.length-1){
+			alert("色・サイズを選択してください。");
+			return;
+		}
+	}
+	var cartAddRadioParam= paramList.split(",");
+	getSkuGoodsParam(cartAddRadioParam[0],cartAddRadioParam[1],cartAddRadioParam[2],cartAddRadioParam[3]);
+}
+function AddCart_trans2(formName){
+  document.cartGoodsAddForm.action="https://" + document.domain + "/shop/cartGoodsAdd.do";
+  return anchorButtonClick(formName);
+}
+
+function siteCatalistAddCart2(formName,s_products){
+  s.linkTrackVars='events,products';
+  s.linkTrackEvents='scAdd,event27';
+  s.products=";"+s_products;
+  s.events='scAdd,event27';
+  s.tl(this,'o','cartadd');
+  setTimeout('AddCart_trans2('+ formName +')',1000);
+}
+
+function getSkuGoodsParam(formName,displayCD,goodsSkuCD,extGoodsSkuCD){
+	document.forms[formName].display_code.value = displayCD;
+	document.forms[formName].goods_sku_code.value = goodsSkuCD;
+	document.forms[formName].ext_goods_sku_code.value = extGoodsSkuCD;
+	document.forms[formName].order_num_string.value = 1;
+	siteCatalistAddCart2(formName,extGoodsSkuCD);
+}
