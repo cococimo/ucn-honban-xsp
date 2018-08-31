7074afa00c904b198414261521982ee6cf943d55
cococimo003 <cococimo003@gmail.com>
2018-08-31 15:55:44 +0900

    ココチモ　みみもとくんLP_CM動画追加＆IE10レイアウト崩れ修正

---
+++ honban/contents/shop/00000003/js/mimimoto_lp/common.js
@@ -0,0 +1,30 @@
+(function($){
+  var ua = window.navigator.userAgent.toLowerCase();
+  if (ua.indexOf('chrome')  !== -1) {
+    $('a[href*="youtube.com/watch"]').magnificPopup({
+     type: 'iframe',
+         iframe: {
+             patterns: {
+                 youtube: {
+                     index: 'youtube.com', 
+                     id: 'v=', 
+                     src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0&mute=1'
+                  }
+             }
+         }   
+   });
+  } else {
+    $('a[href*="youtube.com/watch"]').magnificPopup({
+     type: 'iframe',
+         iframe: {
+             patterns: {
+                 youtube: {
+                     index: 'youtube.com', 
+                     id: 'v=', 
+                     src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0'
+                  }
+             }
+         }   
+   });
+  }   
+})(jQuery);
